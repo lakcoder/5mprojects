ff1a7f0b3eaf42489c5199719b4f76f0fcc3cff9
Lakshya Shukla <anillakshya6@gmail.com>
2021-05-05 22:34:42 +0530

    site under construction

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php $pagetitle = "5M Projects"; ?>
+<html>
+
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
+    <!-- Basic -->
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title><?php echo $pagetitle; ?></title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="5M Projects & Solutions">
+    <meta name="author" content="Lakshya Shukla">
+
+    <!-- Web Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+    <!-- Theme Styles -->
+    <link href="css/main.css" rel="stylesheet" type="text/css"/>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/icon.png">
+</head>
+
+</html>
+
+<div class="overlay"></div>
+<div class="stars" aria-hidden="true"></div>
+<div class="starts2" aria-hidden="true"></div>
+<div class="stars3" aria-hidden="true"></div>
+<main class="main">
+	<section class="contact">
+		<img src="img/5mprojects-logo.png" height="200 rem" alt="5M Projects Logo" />
+		<h2 class="title">This website is under construction.</h2>
+    <h2 class="sub-title">Contact us: <br> +9122 4006 3327 </h2>
+    <p>
+	</section>
+</main>
